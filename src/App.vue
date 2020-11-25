4987ffa2ffb18abba1dfe26a996216b4e745cc65
windowdotonload <275981471@qq.com>
2020-11-25 08:30:53 +0800

    modChange

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+-->
 <template>
   <div id="app">
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    alert("modify version");
+  },
+};
+</script>
 
 <style lang="scss">
 </style>
