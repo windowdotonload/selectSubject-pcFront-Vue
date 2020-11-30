11a0a65fb6fbdf63b3a8c015a3d8e0345fa6b26a
windowdotonload <275981471@qq.com>
2020-11-30 09:18:10 +0800

    stuApplyHistory

--- src/views/student/views/applyhistory.vue
+++ src/views/student/views/applyhistory.vue
@@ -3,8 +3,51 @@
  * @version: 
  * @Author: windowdotonload
 -->
+
 <template>
   <div>
-    <h1>applyhistory</h1>
+    <el-card style="width: 36%">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :timestamp="activity.createdAt | dateFormat"
+        >
+          <p class="content">{{ activity.content }}</p>
+        </el-timeline-item>
+      </el-timeline>
+    </el-card>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    this.getApplay();
+  },
+  data() {
+    return {
+      id: window.sessionStorage.getItem("id"),
+      activities: [],
+    };
+  },
+  methods: {
+    async getApplay() {
+      let applyres = await this.$api.teacherGetApplyHistory({
+        id: this.id,
+      });
+      if (applyres.msg == "success") {
+        this.activities = applyres.data;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.content {
+  font-size: 16px;
+  white-space: pre-wrap;
+  line-height: 150%;
+}
+</style>
